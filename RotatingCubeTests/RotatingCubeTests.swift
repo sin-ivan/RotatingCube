893971c15ec5fc900e46412bbeb0bf408c06ea6e
Ivan Sinitsa <isinitsa@lohika.com>
2015-06-09 14:24:28 +0300

    Initial Commit

---
+++ RotatingCubeTests/RotatingCubeTests.swift
@@ -0,0 +1,35 @@
+//
+//  RotatingCubeTests.swift
+//  RotatingCubeTests
+//
+//  Created by Ivan Sinitsa on 6/9/15.
+//  Copyright © 2015 Ivan Sinitsa. All rights reserved.
+//
+
+import XCTest
+
+class RotatingCubeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
